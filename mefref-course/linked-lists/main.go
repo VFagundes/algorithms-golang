97286be1fec9b409c09406bb04f385f18549f6fa
vinicius <vfagundes>
2024-05-26 18:56:44 +0200

    add complete linked list implementation

--- mefref-course/linked-lists/main.go
+++ mefref-course/linked-lists/main.go
@@ -3,71 +3,108 @@ package main
 import "fmt"
 
 type Node struct {
-	data int
-	next *Node
+	Data int32
+	Next *Node
 }
 type LinkedList struct {
-	Length int
 	Head   *Node
+	Length int32
 }
 
-func (l *LinkedList) Prepend(n *Node) {
-	sec := l.Head
-	l.Head = n
-	n.next = sec
+func New(Value int32) *LinkedList {
+	return &LinkedList{Head: &Node{Data: Value}, Length: 1}
+}
+
+func (l *LinkedList) Prepend(Value int32) int32 {
+	next := l.Head
+	l.Head = &Node{Data: Value}
+	l.Head.Next = next
 	l.Length++
+	return l.Length
 }
-func (l *LinkedList) Delete(value int) {
-	if l.Length == 0 {
-		fmt.Println("empty list")
-		return
-	}
-	previous := l.Head
-	if previous.data == value {
-		l.Head = previous.next
-		l.Length--
-		return
-	}
-	for previous.next.data != value {
-		previous = previous.next
-		if previous.next == nil {
-			fmt.Println("value not found")
-			return
-		}
-	}
-	previous.next = previous.next.next
-	//1,2,3,4 param:3
-	//
-	l.Length--
-}
-
-func New(data int) LinkedList {
-	return LinkedList{Head: &Node{data: data}, Length: 1}
-}
-func (l *LinkedList) Print() {
+func (l *LinkedList) PrintList() {
 	current := l.Head
 	for {
-		fmt.Printf("value: %d list size: %d\n", current.data, l.Length)
-
-		current = current.next
-		if current == nil {
+		fmt.Printf("Node value: %d\n", current.Data)
+		if current.Next == nil {
 			break
 		}
-
+		current = current.Next
 	}
+	fmt.Printf("LinkedList Length: %d\n", l.Length)
 }
-func main() {
-	l := New(1)
-	l.Prepend(&Node{data: 2})
-	l.Prepend(&Node{data: 3})
-	l.Prepend(&Node{data: 4})
-	l.Prepend(&Node{data: 5})
-	l.Prepend(&Node{data: 6})
 
-	l.Print()
-	fmt.Println("------")
-	// l.Delete(3)
-	l.Delete(6)
-	// l.Delete(4)
-	l.Print()
+func (l *LinkedList) Delete(Value int32) int32 {
+	if l.Length == 0 {
+		return 0
+	}
+	if l.Head.Data == Value {
+		l.Head = l.Head.Next
+		l.Length--
+		return l.Length
+	}
+	previous := l.Head
+	//-1 0 1
+	for previous.Next != nil && previous.Next.Data != Value {
+		previous = previous.Next
+	}
+	if previous.Next == nil {
+		fmt.Println("No values found")
+	} else {
+		previous.Next = previous.Next.Next
+		l.Length--
+	}
+
+	return l.Length
+}
+
+func (l *LinkedList) InsertAtTheEnd(Value int32) int32 {
+	if l.Length == 0 {
+		l = New(Value)
+		return l.Length
+	}
+
+	lastNode := l.Head
+	for lastNode.Next != nil {
+		lastNode = lastNode.Next
+	}
+	lastNode.Next = &Node{Data: Value}
+	l.Length++
+
+	return l.Length
+}
+func (l *LinkedList) Reverse() {
+	var prev *Node
+	var next *Node
+	current := l.Head
+	for current != nil {
+		next = current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	l.Head = prev
+
+}
+
+func main() {
+	l := New(-1)
+	i := 1
+	for i = range 4 {
+		l.Prepend(int32(i))
+	}
+	l.PrintList()
+	l.Reverse()
+	l.PrintList()
+	// l.Reverse()
+
+	// l.Prepend(0)
+	// l.Prepend(-1)
+	// l.PrintList()
+	// l.Delete(1)
+	// l.Delete(12)
+	// l.Delete(111)
+	// l.InsertAtTheEnd(2)
+
+	fmt.Println("print linked list code")
 }
